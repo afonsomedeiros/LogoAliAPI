1fe6cb047c9c869a0e33cf021161d54e8a93c65c
Afonso Medeiros <afonso.b.medeiros@gmail.com>
2021-11-04 04:07:38 -0300

    update application.py

--- application.py
+++ application.py
@@ -2,4 +2,17 @@ from app import create_app
 
 application = create_app()
 
+
+html = '''
+<html>
+    <head>
+        <title>LogoAliAPI</title>
+    </head>
+    <body>
+        <h1>LogoAliAPI!</h1>
+    </body>
+</html>
+'''
+application.add_url_rule('/', 'index', (lambda:html))
+
 application.run()
