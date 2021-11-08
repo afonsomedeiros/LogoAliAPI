9251dbc04144efeab94c4e50b2e0680c4815660f
Afonso Medeiros <afonso.b.medeiros@gmail.com>
2021-11-08 02:14:05 -0300

    update

--- app/extensions/sqlalchemy_ext.py
+++ app/extensions/sqlalchemy_ext.py
@@ -7,7 +7,7 @@ orm = SQLAlchemy()
 
 def install(app):
     #app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:////{ROOT_PATH}/database.db"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://afonso2k:LOGOALIAPI123@afonso2k.mysql.pythonanywhere-services.com/LOGOALI"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://afonso2k:LOGOALIAPI123@afonso2k.mysql.pythonanywhere-services.com/afonso2k$LOGOALI"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     orm.init_app(app)
     app.db = orm
